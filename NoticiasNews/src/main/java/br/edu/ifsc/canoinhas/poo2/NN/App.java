b040ca52a3b476de1fe6fdb4d52055b07ec17f7a
guigui03262 <42380622+guigui03262@users.noreply.github.com>
2021-03-15 20:05:13 -0300

    Add files via upload

---
+++ NoticiasNews/src/main/java/br/edu/ifsc/canoinhas/poo2/NN/App.java
@@ -0,0 +1,28 @@
+package br.edu.ifsc.canoinhas.poo2.NN;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class App extends Application {
+
+    private static Stage stage;
+
+    @Override
+	public void start(Stage stge) {
+		stage = stge;
+		stage.setScene(FXMLUtil.loadScene("principal"));
+		stage.setTitle("NN");
+		stage.show();
+	}
+
+    public static void setRoot(String fxml) throws IOException {
+    	stage.setScene(FXMLUtil.loadScene(fxml));
+    }
+
+    /*private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }*/
+
+}
